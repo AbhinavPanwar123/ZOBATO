45b81a5ffe713a5c52a22b17a265947cb321241c
abhinavpanwar <abhinavpanwar0606@gmail.com>
2024-09-05 15:42:00 +0530

    Remove Backend directory and its contents from repository

--- Frontend/Backend/routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const sendReservation = require ('../Controller/authController')
-
-/* Post Reservation */ 
-router.post('/send',sendReservation);
-
-module.exports = router;
